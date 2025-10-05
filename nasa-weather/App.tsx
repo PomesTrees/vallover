430a4dc00e2d09e2dcd9f74f6418c90a1cd8ac6c
andres-villaa <sloweight39729@gmail.com>
2025-10-04 23:35:10 -0600

    Charts

--- nasa-weather/App.tsx
+++ nasa-weather/App.tsx
@@ -1,42 +1,24 @@
 import React from "react";
-import { NativeBaseProvider, Box, Text, Button, VStack } from "native-base";
+import { useColorScheme } from "react-native";
+import { NativeBaseProvider, Box, Text, Button, VStack, HStack, Spacer} from "native-base";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
-// import { LineChart } from "react-native-gifted-charts";
-// import { LineChart as RNCKLineChart } from "react-native-chart-kit";
-// import { Dimensions } from "react-native";
 import CustomWeatherChart from "./components/CustomWeatherChart";
 
 export default function App() {
+  const scheme = useColorScheme();
+  const bgColor = scheme === 'dark' ? '#18181b' : 'white';
+  const cardColor = scheme === 'dark' ? '#343438ff' : 'white';
 
   return (
     <GluestackUIProvider config={config}>
       <NativeBaseProvider>
-        <Box safeArea bg="primary.600" px="4" py="3">
-          <Text color="white" fontSize="lg" fontWeight="bold">
-            NASA Weather — UI online ✅
-          </Text>
-        </Box>
-
-        <VStack space="4" p="4">
-          <Box bg="gray.100" rounded="lg" p="4" shadow="1">
-            <Text fontSize="md" mb="2">
-              NativeBase + Gluestack UI + Gifted Charts integrated! 🚀
-            </Text>
-            <Text fontSize="sm" color="gray.600">
-              Testing chart component below:
-            </Text>
-          </Box>
-          
-          <Button onPress={() => console.log("Button works!")}>
-            Test Button
-          </Button>
-
-          <Box bg="white" rounded="lg" p="4" shadow="2" mt="4">
-            <Text fontSize="md" fontWeight="bold" mb="4">
-              Bezier Line Chart (Gifted Charts)
-            </Text>
-            <CustomWeatherChart />
+        <VStack space="4" p="4" bg={bgColor} width="100%" height="100%">
+          <Box rounded="lg" p="4" shadow="2" bg={cardColor}>
+            <HStack w="100%" pl="4" pr="4" justifyContent="center" flexWrap="wrap">
+              <CustomWeatherChart />
+              <CustomWeatherChart />
+            </HStack>
           </Box>
         </VStack>
       </NativeBaseProvider>
--- nasa-weather/components/CustomWeatherChart.tsx
+++ nasa-weather/components/CustomWeatherChart.tsx
@@ -4,33 +4,33 @@ import { LineChart } from "react-native-gifted-charts";
 
 const CustomWeatherChart = () => {
   return (
-    <Box bg="white" rounded="lg" p="4" shadow="2" mt="4">
-      <Text fontSize="md" fontWeight="bold" mb="4">
-        Bezier Line Chart (Gifted Charts)
+  <Box bg="#27272a" w="400px" h="220px" rounded="lg" m={2} p="2" shadow="2" display="flex" flexDirection="column" alignItems="center" justifyContent="center" borderWidth={2} borderColor="#e81f1fff">
+      <Text fontSize="md" fontWeight="bold" mb="4" color={"#ffffff"}>
+        Bezier Line Chart
       </Text>
-      <LineChart
+        <Box w="100%" h="140px" alignItems="center" justifyContent="center">
+          <LineChart
             data={[{ value: 2, label: 'Mon' }, { value: 3, label: 'Tue' }, { value: 0, label: 'Wed' }, { value: 3, label: 'Thu' }, { value: 7, label: 'Fri' }, { value: 2, label: 'Sat' }, { value: 5, label: 'Sun' }]}
             curved
             areaChart
-            height={150}
-            width={330}
+            height={120}
+            width={340}
             color="#0288d1"
             thickness={3}
             dataPointsColor="#0288d1"
             showVerticalLines
             hideRules
-            // rulesType="solid"
-            // rulesColor="#d7d7d7ff"
-            // verticalLinesColor="#b3e5fc"
-            xAxisColor="#ffffffff"
-            yAxisColor="#ffffffff"
-            startFillColor="#4fc3f7"
-            endFillColor="#e0f7fa"
+            verticalLinesColor="#3f3f45ff"
+            xAxisColor="#27272a"
+            yAxisColor="#27272a"
+            startFillColor="#2178a1ff"
+            endFillColor="#26505fff"
             startOpacity={0.9}
             endOpacity={0.1}
             xAxisLabelTextStyle={{fontSize:10, color:'#ffffffff', fontWeight:'500'}}
             yAxisTextStyle={{fontSize: 8, color: '#818181ff'}}
-      />
+          />
+        </Box>
     </Box>
   );
 };
